246dc8698298a3cd4abb5ff4b30c369d3d0b3364
kavirasanE <einsteinkavi98@gmail.com>
2024-04-27 16:50:34 +0530

    changes

--- TalentXworks1/frontend/src/pages/Aboutus.jsx
+++ TalentXworks1/frontend/src/pages/Aboutus.jsx
@@ -1,25 +1,26 @@
 import React from 'react'
-
+import figure from "../assets/f.svg";
+import service2 from "../assets/service2.png"
 const Aboutus = () => {
     return (
         <div>
             <div className='flex flex-col justify-center px-4  items-center'>
-                <p className='font-bold text-4xl py-4'>About US</p>
-                <p className='text-md px-5'>We are TalenX Works Technology Solutions, at here we value human, organizational, and operational intelligence, not just artificial.</p>
-                <p className='flex flex-col gap-5 mt-5'>
-                    <span className='bg-green-400 p-4 text-white text-center font-bold '>Speak to an Expert</span>
+                <p className='font-bold text-4xl lg:text-6xl py-4'>About US</p>
+                <p className='text-md lg:text-lg px-5 text-center '>We are TalenX Works Technology Solutions, at here we value human,<br/> organizational, and operational intelligence, not just artificial.</p>
+                <p className='flex flex-col lg:flex-row justify-center items-center gap-5 my-5'>
+                    <span className='bg-green-400 p-4 px-10 text-white text-center font-bold '>Speak to an Expert</span>
                     <span className=' font-bold py-5'>Learn more about services</span>
                 </p>
-                <div>
+                <div className='w-full'>
                     <img fetchpriority="high" decoding="async" data-attachment-id="404" data-permalink="https://kasadara.com/?attachment_id=404" data-orig-file="https://i0.wp.com/kasadara.com/wp-content/uploads/2022/11/about.png?fit=2340%2C1100&amp;ssl=1" data-orig-size="2340,1100" data-comments-opened="1" data-image-meta="{&quot;aperture&quot;:&quot;0&quot;,&quot;credit&quot;:&quot;&quot;,&quot;camera&quot;:&quot;&quot;,&quot;caption&quot;:&quot;&quot;,&quot;created_timestamp&quot;:&quot;0&quot;,&quot;copyright&quot;:&quot;&quot;,&quot;focal_length&quot;:&quot;0&quot;,&quot;iso&quot;:&quot;0&quot;,&quot;shutter_speed&quot;:&quot;0&quot;,&quot;title&quot;:&quot;&quot;,&quot;orientation&quot;:&quot;0&quot;}" data-image-title="about" data-image-description="" data-image-caption="" data-medium-file="https://i0.wp.com/kasadara.com/wp-content/uploads/2022/11/about.png?fit=300%2C141&amp;ssl=1" data-large-file="https://i0.wp.com/kasadara.com/wp-content/uploads/2022/11/about.png?fit=900%2C423&amp;ssl=1" width="2340" height="1100" src="https://i0.wp.com/kasadara.com/wp-content/uploads/2022/11/about.png?fit=2340%2C1100&amp;ssl=1" class="attachment-full size-full wp-image-404" alt="" srcset="https://i0.wp.com/kasadara.com/wp-content/uploads/2022/11/about.png?w=2340&amp;ssl=1 2340w, https://i0.wp.com/kasadara.com/wp-content/uploads/2022/11/about.png?resize=300%2C141&amp;ssl=1 300w, https://i0.wp.com/kasadara.com/wp-content/uploads/2022/11/about.png?resize=1024%2C481&amp;ssl=1 1024w, https://i0.wp.com/kasadara.com/wp-content/uploads/2022/11/about.png?resize=768%2C361&amp;ssl=1 768w, https://i0.wp.com/kasadara.com/wp-content/uploads/2022/11/about.png?resize=1536%2C722&amp;ssl=1 1536w, https://i0.wp.com/kasadara.com/wp-content/uploads/2022/11/about.png?resize=2048%2C963&amp;ssl=1 2048w, https://i0.wp.com/kasadara.com/wp-content/uploads/2022/11/about.png?resize=1200%2C564&amp;ssl=1 1200w, https://i0.wp.com/kasadara.com/wp-content/uploads/2022/11/about.png?w=1800&amp;ssl=1 1800w" sizes="(max-width: 900px) 100vw, 900px" />
                 </div>
             </div>
-            <div className='flex flex-col justify-center items-center  gap-2 mt-10'>
+            <div className='flex flex-col justify-center items-center  lg:items-start lg:ml-10 gap-2 my-10'>
                 <p className='uppercase text-green-400 font-bold'>ABOUT TalentXworks</p>
-                <p className='font-bold text-2xl text-center'>We excel at the <br className='text-nowrap' />intersection of business<br /> and technology </p>
-                <p className='hidden'>We value human, organizational, and operational intelligence, not just artificial.</p>
+                <p className='font-bold text-2xl lg:text-3xl text-center lg:text-start lg:flex lg:justify-between lg:items-center lg:gap-40'>We excel at the <br className='lg:hidden text-nowrap' />intersection of business<br /> and technology 
+                <span className='hidden lg:flex lg:items-center text-sm font-light lg:gap-10'>We value human, organizational, and operational <br/>intelligence, not just artificial. <button className='font-bold px-5 p-3 border mx-4'> View More</button></span></p>
             </div>
-            <div className='flex flex-col gap-10 py-4'>
+            <div className='flex flex-col lg:flex-row gap-10 lg:gap-2 py-4 lg:my-10'>
                 <div className='mx-4 border-2 shadow-2xl shadow-gray-200'>
                     <img decoding="async" data-attachment-id="1728" data-permalink="https://kasadara.com/?attachment_id=1728" data-orig-file="https://i0.wp.com/kasadara.com/wp-content/uploads/2022/12/Group-17.jpg?fit=740%2C320&amp;ssl=1" data-orig-size="740,320" data-comments-opened="1" data-image-meta="{&quot;aperture&quot;:&quot;0&quot;,&quot;credit&quot;:&quot;&quot;,&quot;camera&quot;:&quot;&quot;,&quot;caption&quot;:&quot;&quot;,&quot;created_timestamp&quot;:&quot;0&quot;,&quot;copyright&quot;:&quot;&quot;,&quot;focal_length&quot;:&quot;0&quot;,&quot;iso&quot;:&quot;0&quot;,&quot;shutter_speed&quot;:&quot;0&quot;,&quot;title&quot;:&quot;&quot;,&quot;orientation&quot;:&quot;0&quot;}" data-image-title="Group 17" data-image-description="" data-image-caption="" data-medium-file="https://i0.wp.com/kasadara.com/wp-content/uploads/2022/12/Group-17.jpg?fit=300%2C130&amp;ssl=1" data-large-file="https://i0.wp.com/kasadara.com/wp-content/uploads/2022/12/Group-17.jpg?fit=740%2C320&amp;ssl=1" width="740" height="320" src="https://i0.wp.com/kasadara.com/wp-content/uploads/2022/12/Group-17.jpg?fit=740%2C320&amp;ssl=1" class="attachment-full size-full wp-image-1728" alt="" srcset="https://i0.wp.com/kasadara.com/wp-content/uploads/2022/12/Group-17.jpg?w=740&amp;ssl=1 740w, https://i0.wp.com/kasadara.com/wp-content/uploads/2022/12/Group-17.jpg?resize=300%2C130&amp;ssl=1 300w" sizes="(max-width: 740px) 100vw, 740px"></img>
                     <p className='text-center pt-8 font-bold text-xl'>What we do?</p>
@@ -36,31 +37,68 @@ const Aboutus = () => {
                     <p className='text-center text-md pb-10'>We can help you with your technology needs</p>
                 </div>
             </div>
-            <div className='px-4'>
+            <div className='px-4 my-20'>
                 <p className='text-green-500 font-bold'>WHAT WE DO</p>
                 <p className='font-bold text-2xl py-2'>We will help you overcome your technology challenges</p>
+                <div className='lg:flex lg:flex-row lg:items-center lg:justify-between '>
                 <p>
-                    <span className='gap-2 flex flex-row justify-between items-center my-4'><span className=' bg-green-400  py-1 px-1'></span>Our best-of-breed IT solutions will drive efficiency and change</span>
-                    <span className='gap-2 flex flex-row justify-between items-center my-4'><span className=' bg-green-400  py-1 px-1'></span> Refine your data for better-informed decision making </span>
-                    <span className='gap-2 flex flex-row justify-between items-center my-4'><span className=' bg-green-400  py-1 px-1'></span> Our smart solutions contribute to revenue growth with significant improvements in business processes and workflows.</span>
-                    <span className='gap-2 flex flex-row justify-between items-center my-4'><span className=' bg-green-400  py-1 px-1'></span> Protect your business with our leading business continuity solutions</span>
+                    <span className='gap-2 flex flex-row justify-between lg:justify-start items-center lg:mx-10 my-4'><span className='bg-green-400  py-1 px-1'></span>Our best-of-breed IT solutions will drive efficiency and change</span>
+                    <span className='gap-2 flex flex-row justify-between  lg:justify-start items-center lg:mx-10  my-4'><span className=' bg-green-400  py-1 px-1'></span> Refine your data for better-informed decision making </span>
+                    <span className='gap-2 flex flex-row justify-between  lg:justify-start items-center lg:mx-10  my-4'><span className=' bg-green-400  py-1 px-1'></span> Our smart solutions contribute to revenue growth with significant improvements in business processes and workflows.</span>
+                    <span className='gap-2 flex flex-row justify-between  lg:justify-start items-center lg:mx-10  my-4'><span className=' bg-green-400  py-1 px-1'></span> Protect your business with our leading business continuity solutions</span>
                 </p>
+                <img src={service2}/>
+                </div>
             </div>
 
 
-            <div>
-            <div className='text-center flex flex-col justify-center items-center'>
-                <p className=' font-bold text-2xl'>Our Mission</p>
-                <p className='text-center px-5'>Building on our technologies and competencies creating value for our customers. We’ll achieve this by focusing on the intersection of our client’s emerging needs and the acceleration of business and technological change.</p>
-            </div>
-            <div>
+<div className='flex flex-col lg:flex-row shadow-xl lg:gap-10'>
 
+            <div className='text-center flex flex-col justify-center items-center mt-7'>
+                <p className=' font-bold text-2xl my-4'>Our Mission</p>
+                <p className='text-center px-5 pb-6'>Building on our technologies and competencies creating value for our customers. We’ll achieve this by focusing on the intersection of our client’s emerging needs and the acceleration of business and technological change.</p>
             </div>
-                <p>Our Values</p>
-                <p>Our values are the guiding principles upon which Kasadara was founded and how we strive to conduct our business on a daily basis. Values establish our view of the world as we shape the future. They determine how we treat each other.</p>
-                <p>Our Culture</p>
-                <p>Our drive for exceptional service delivery is built on the belief that we are nothing if you are not satisfied with us. Our passion for helping you achieve your goals, no matter what, is what truly differentiates us from our competitors.</p>
+
+            <div className='text-center flex flex-col justify-center items-center mt-7'>
+                <p className=' font-bold text-2xl my-4'>Our Mission</p>
+                <p className='text-center px-5 pb-6'>Building on our technologies and competencies creating value for our customers. We’ll achieve this by focusing on the intersection of our client’s emerging needs and the acceleration of business and technological change.</p>
             </div>
+            <div className='text-center flex flex-col justify-center items-center mt-7 '>
+                <p className=' font-bold text-2xl my-4'>Our Mission</p>
+                <p className='text-center px-5 pb-6'>Building on our technologies and competencies creating value for our customers. We’ll achieve this by focusing on the intersection of our client’s emerging needs and the acceleration of business and technological change.</p>
+            </div>
+
+</div>
+            <div className='flex flex-col justify-center items-center text-center'>
+                <p className='text-sm mt-5 font-bold text-green-400'>WHY WORK WITH US</p>
+                <p className='font-bold text-xl px-4'>6 reasons why you should partner with Kasadara</p>
+                <p className='my-5 text-center'>Our customers are our number one priority and we strive to deliver them a service experience that’s second to none.</p>
+            </div>
+
+            <div className='grid grid-cols-2 lg:flex lg:justify-between lg:mx-10 my-10 gap-6 items-center justify-center grid-flow-row '>
+                <div className='flex flex-col items-center gap-2 justify-center'>
+                    <img src={figure} className='text-green-400 bg-green-300 p-10 rounded-full' />
+                    <p className='font-semibold '>Quick Response</p>
+                </div>
+                <div className='flex flex-col items-center justify-center'>
+                    <img src={figure} className='text-green-400 bg-green-300 p-10 rounded-full' />
+                    <p className='font-semibold '>Quick Response</p>
+                </div>
+                <div className='flex flex-col items-center justify-center'>
+                    <img src={figure} className='text-green-400 bg-green-300 p-10 rounded-full' />
+                    <p className='font-semibold '>Quick Response</p>
+                </div>
+                <div className='flex flex-col items-center justify-center'>
+                    <img src={figure} className='text-green-400 bg-green-300 p-10 rounded-full' />
+                    <p className='font-semibold '>Quick Response</p>
+                </div>
+                <div className='flex flex-col items-center justify-center'>
+                    <img src={figure} className='text-green-400 bg-green-300 p-10 rounded-full' />
+                    <p className='font-semibold '>Quick Response</p>
+                </div>
+            </div>
+
+
         </div>
     )
 }
